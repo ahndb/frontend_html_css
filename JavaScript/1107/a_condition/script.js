8dc0a59e7ca0832e0dba89b91aeea025c574e420
asi0000 <asi0000@nate.com>
2023-11-07 15:52:50 +0900

    20231107 htmlcss2

--- JavaScript/1107/a_condition/script.js
+++ JavaScript/1107/a_condition/script.js
@@ -1,3 +1,4 @@
+{
 //  자바스크립트의 구성 요소
 //  조건문, 반복문, 함수, 이벤트 등
 
@@ -18,7 +19,7 @@ if (조건) {
 
 */
 
-// 조건은 보통 비교 연산자
+// 조건은 보통 비교 연산자를 사용해 표현
 // (==, !=, ===, !==, >, <, <=, >=)
 // 리턴값(결과값)은 boolean타입(true, false)
 
@@ -70,7 +71,8 @@ let childsAllowance;
 // 쇼핑을 완료 한 경우: 용돈을 10
 // 완료하지 않은 경우: 용돈을 5
 
-if (shoppingDone === false) {
+// 값에 false면 생략가능
+if (shoppingDone) {
   childsAllowance = 5;
 } else {
   childsAllowance = 10;
@@ -119,3 +121,171 @@ if (age < 13) {
 } else {
   console.log('성인 입니다.');
 }
+
+// 조건식에 true와 false값과의 비교(===, !==)
+// boolean 타입에서 false값으로 인식되는 값
+// : false, 0, '', null, undefined
+
+// 위의 값을 제외하고는 true값을 반환하기 때문에
+// 변수가 참인지 또는 값이 존재하는지를 테스트하기 위해서
+// 변수 이름 그 자체를 사용할 수 있음.
+
+let cheese = 'cheddar'; //'' 안에 값이 있어서 true로 인식됨
+
+if (cheese) {
+  console.log("cheese avilable for making cheese on toast.");
+} else {
+  console.log("No cheese on toast for you today");
+}
+
+//! 4. switch문
+// 여러 가지 경우 중 하나를 선택하는데 사용
+// 표현식의 값을 확인하고 해당 값과 일치하는 case 문의 코드 블록을 실행
+
+// switch문은 여러 가지 가능한 케이스 중 하나를 선택하여
+// 해당하는 코드 블록을 실행하는 제어 구조
+
+// switch문의 기본적인 문법
+/*
+switch (식) {
+  case 값1 : 
+    식이 값1과 일치할 때 실행되는 코드
+    break;
+  case 값2 : 
+    식이 값2과 일치할 때 실행되는 코드
+    break;
+  ...
+  default:
+    식이 어떤 값과도 일치하지 않을 때 실행되는 코드
+}
+*/ 
+
+// break문(switch문에서 필수는 아님)
+// : 이전의 선택이 표현식이나 값과 일치한다면
+// : , 브라우저는 해당 코드 블록에서 실행을 멈추고
+// : , switch문 아래에 있는 코드로 이동
+
+// default문(switch문에서 필수는 아님)
+// : 어떤 case와도 일치하지 않는다면 실행하는 코드 블록
+
+let fruit = 'banana';
+
+switch (fruit) {
+  case 'banana':
+    console.log("I am a banana.");
+    break;
+  case 'apple':
+    console.log("I am a apple.");
+    break; // break 안넣으면 뒷 내용 다 실행됨
+  case 'cherry':
+    console.log("I am a cherry.");
+    break;
+  default:
+    console.log("i don't know.");
+}
+
+//* 날씨 예보 예제 작성하기
+
+const select = document.querySelector('select');
+const para = document.querySelector('p');
+
+select.onchange = setWeather;
+
+function setWeather() {
+  const choice = select.value;
+
+  if (choice === 'sunny') {
+  para. textContent = "It's sunny day";
+  } else if (choice === 'rainy') {
+  para.textContent = "It's rainy day";
+  } else if (choice === 'snowing') {
+  para.textContent = "It's snowing day";
+  } else if (choice === 'overcast') {
+  para.textContent = "It's overcast day";
+  } else {
+    para.textContent = "Please select weather";
+  }
+}
+
+// switch 문 연습하기
+
+
+//! 5. 삼황(조건) 연산자
+// a; 1항, a = b; 2항
+// 조건에 따라 값을 반환하는 JS에서 세 개의
+// 피연산자를 가지는 유일한 연산자
+// : if-else문을 더 간결하게 표현
+
+// 삼황 연산자의 기본적인 문법
+// 조건 ? 표현식1 : 표현식2
+
+// 주어진 조건을 평가한 후
+// , 그 결과가 true면 표현식1을, false면 표현식2를 실행
+
+/*  
+if (조건) {
+  표현식1
+} else {
+  표현식2
+}
+*/
+
+number = 15;
+let message = number > 10 
+  ? 'The number is greater than 10' 
+  : 'The number is not greater than 10';
+
+  console.log(message);
+
+let score = 85;
+let result = score >= 50 ? 'Passed' : 'Failed';
+console.log(result);
+
+//! 중첩된 if-else문과 삼황 연산자의 비교
+
+/* 
+? 중첩된 if-else문 
+if (조건1) {
+  조건1이 참일 때 실행될 코드
+  if (조건2) {
+    조건1과 조건2 모두 참일 때 실행될 코드
+  } else {
+    조건1은 참이지만, 조건2는 거짓일 때 실행될 코드
+  }
+} else {
+  조건1이 거짓일 때 실행될 코드
+}
+*/
+
+// 50점 이상일 경우 시험에서 통과
+// 90점 이상일 경우 A
+// 80점 이상일 경우 B
+// 70점 이상일 경우 C
+// 그외의 경우 D
+
+// 50점 미만일 경우 불합격
+
+score = 90;
+
+if (score >= 50) {
+  console.log("Passed");
+
+  if (score >= 90) {
+    console.log("A")
+  } else if (score >= 80) {
+    console.log("B")
+  } else if (score >= 70) {
+    console.log("C")
+  } else {
+    console.log("Failed")
+  }
+}
+
+}
+
+// 위 중첩 if문을 삼항 연산자로 변환
+score = 85;
+
+let grade = ;
+
+console.log(grade);
