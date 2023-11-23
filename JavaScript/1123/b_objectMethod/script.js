f8c28a233b7805f3e90c184a6709cd473885d147
asi0000 <asi0000@nate.com>
2023-11-23 15:17:37 +0900

    231123 1

---
+++ JavaScript/1123/b_objectMethod/script.js
@@ -0,0 +1,19 @@
+// 자바스크립트 내장 객체(Built-in Objects)
+
+//! 3. JSON 객체
+// JavaScript Object Notation (자바스크립트 객체 표기법)
+
+//? JSON 구조
+// : JSON 내부에는 JS에서 사용할 수 있는 기본 데이터 타입인 문자열, 숫자, 배열, 불리언, 그 밖의 다른 객체 또한 포함 가능
+
+// 하나의 자료
+let jsonData = {
+  'name': '안상현',
+  'age': 31,
+  'job': 'developer',
+  'hobby': {
+    'frist': 'climbing',
+    'second': 'music'
+  },
+  'lecture': ['평일', '주말' ]
+};
