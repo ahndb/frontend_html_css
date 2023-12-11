3958c018b4426caf0226513974913f0bb32ba7c7
asi0000 <asi0000@nate.com>
2023-12-12 00:27:16 +0900

    231211

---
+++ TypeScript/src/1208/GenericPractice/main.ts
@@ -0,0 +1,50 @@
+{
+// 타입 속성 정의
+// : Task 타입 정의
+// : 제네릭 타입 T를 사용하여 지정
+type Task<T> = {
+  id: number;
+  content: T;
+}
+
+// TaskManager 클래스 정의
+// : T 타입의 할 일을 관리
+class TaskManager<T> {
+  //? 멤버 변수 지정: private 설정
+  // 할 일 목록을 저장하는 배열(tasks)
+  // 다음 할 일의 고유 ID(nextId)
+  private tasks!: [];
+  private nextId!: number;
+
+  //? 생성자
+  // 초기 할 일 목록은 비어 있음
+  // ID는 1부터 시작
+  constructor(tasks: [], nextId: number){
+    this.tasks = []; 
+    this.nextId = 1;
+  }
+  }
+  //? 할 일 추가 함수
+  
+  let addTask = document.querySelector('#add-task')
+  let newTask = document.querySelector('#new-task')
+
+  addTask.addEventListener("click", newTask);
+
+  // function addtask() {
+  //   addTask.addEventListener("click", onclick);
+  // }
+  
+
+  //? 할 일 제거 함수
+  function removeTask () {
+
+  }
+  //? 할 일 목록을 화면에 렌더링하는 함수
+  function renderTask () {
+
+  }
+
+
+// 웹 페이지가 로드될 때 실행되는 함수
+}
